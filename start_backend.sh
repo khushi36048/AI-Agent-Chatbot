84fb9455c7c5214515ff480d293b097461d5d134
Khushi Gupta <khushigupta36048@gmail.com>
2025-06-03 10:21:16 +0530

    Add start commands for Render

---
+++ start_backend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# uvicorn backend:app --host 0.0.0.0 --port 10000
+uvicorn backend:app --host 127.0.0.1 --port 9999 
---
+++ start_frontend.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+streamlit run frontend.py --server.port 10001 --server.address 127.0.0.1
